bb1b406c9b12ae4daefc7623219d16a38fb1dd80
annaheliaoliveira-wq <annaheliaoliveira@gmail.com>
2026-05-24 18:18:51 -0300

    Update tasks.js

--- app/models/tasks.js
+++ app/models/tasks.js
@@ -1,1 +1,44 @@
+class Task {
+    constructor(titulo, descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+}
 
+class TaskModel {
+
+    constructor() {
+        this.tasks = [];
+    }
+
+    adicionar(titulo, descricao) {
+        const tarefa =
+        new Task(
+            titulo,
+            descricao
+        );
+
+        this.tasks.push(tarefa);
+    }
+
+    listar() {
+        return this.tasks;
+    }
+
+    concluir(index) {
+
+        this.tasks[index].concluida =
+        !this.tasks[index].concluida;
+
+    }
+
+    excluir(index) {
+
+        this.tasks.splice(index,1);
+
+    }
+
+}
+
+window.TaskModel = TaskModel;
