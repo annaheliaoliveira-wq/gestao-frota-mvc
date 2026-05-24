002441bc04c55efd32959cebe3efa2977831d60d
annaheliaoliveira-wq <annaheliaoliveira@gmail.com>
2026-05-24 18:15:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+alert("JavaScript funcionando");
 
+alert(typeof TaskModel);
+
+alert(typeof View);
+
+alert(typeof Controller);
+
+const model = new TaskModel();
+
+const view = new View();
+
+window.controller =
+new Controller(
+model,
+view
+);
+
+const botao =
+document.getElementById(
+"btnAdicionar"
+);
+
+botao.onclick =
+function(){
+
+controller.adicionar();
+
+};
