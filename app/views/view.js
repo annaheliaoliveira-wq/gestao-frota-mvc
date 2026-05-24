9b3cf4fc3d2213a81d635690323f60ecec752fd5
annaheliaoliveira-wq <annaheliaoliveira@gmail.com>
2026-05-24 18:20:25 -0300

    Update view.js

--- app/views/view.js
+++ app/views/view.js
@@ -1,1 +1,45 @@
+class View {
 
+    renderizar(tasks){
+
+        const lista =
+        document.getElementById("lista");
+
+        lista.innerHTML="";
+
+        tasks.forEach((task,index)=>{
+
+            lista.innerHTML += `
+
+            <li>
+
+            ${task.titulo}
+            - ${task.descricao}
+
+            ${task.concluida ?
+            "✔ Concluída" :
+            "Pendente"}
+
+            <button onclick=
+            "controller.concluir(${index})">
+
+            Concluir
+
+            </button>
+
+            <button onclick=
+            "controller.excluir(${index})">
+
+            Excluir
+
+            </button>
+
+            </li>
+            `;
+        });
+
+    }
+
+}
+
+window.View = View;
